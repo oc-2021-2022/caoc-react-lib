14df770266895dcf34f871e13e3a0d83178d9ddd
Callan <cerveauxallanjean@gmail.com>
2022-04-20 14:58:19 +0200

    Test Select component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import { ExampleComponent, Option, OptionList, Select } from 'caoc-react-lib'
+import React, { FormEvent, useEffect, useState } from 'react'
 
-import { ExampleComponent } from 'caoc-react-lib'
-import 'caoc-react-lib/dist/index.css'
+import { OptionProps } from '../../src/select/Option'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+const App = (): JSX.Element => {
+  const [testForm, setTestForm] = useState({
+    option: ''
+  })
+  const [selectedOption, setSelectedOption] = useState<string>('')
+
+  const [options, setOptions] = useState<OptionProps[]>([])
+  useEffect(() => {
+    const optionList = [
+      {
+        text: 'foo',
+        value: 'bar'
+      },
+      {
+        text: 'bar',
+        value: 'foo'
+      }
+    ]
+    setOptions(optionList)
+  }, [])
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    const { value } = event.target
+    setSelectedOption(value)
+    console.log(selectedOption)
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    setTestForm({ ...testForm, option: selectedOption })
+    console.log(testForm)
+  }
+
+  return (
+    <>
+      <ExampleComponent text="Create React Library Example 😄 Michel" />
+      <form onSubmit={handleSubmit}>
+        <Select
+          name="Not children"
+          label="Array Options"
+          required
+          options={options} />
+        <br />
+        <Select name="Children options" label="Children Option List" handleChange={handleChange}>
+          <OptionList options={options}/>
+        </Select>
+        <br />
+        <Select name="Children options" label="Children Option" handleChange={handleChange}>
+          <Option text='option 1' value='value 1'/>
+          <Option text='option 2' value='value 2'/>
+          <Option text='option 3' value='value 3'/>
+        </Select>
+        <br />
+        <button type='submit'>Send</button>
+      </form>
+    </>
+  )
 }
+
+
 
 export default App
