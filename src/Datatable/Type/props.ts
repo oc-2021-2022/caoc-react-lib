5ce24da82d65d1a26437d8f57d69bde1ae757338
Callan <cerveauxallanjean@gmail.com>
2022-05-16 16:39:49 +0200

    Typing for props

---
+++ src/Datatable/Type/props.ts
@@ -0,0 +1,13 @@
+type DefaultProps = {
+  key: string
+  class?: string
+}
+
+export type HeaderGroupsProps = DefaultProps & {}
+
+export type HeadersProps = DefaultProps & {
+  colSpan: number | undefined
+}
+
+export type RowProps = DefaultProps & {}
+export type CellProps = DefaultProps & {}
