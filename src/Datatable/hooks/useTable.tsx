796dfc31952a5b1c467fcbd02927054788a74529
Callan <cerveauxallanjean@gmail.com>
2022-06-17 16:43:32 +0200

    Fix, temp fix not working  try again

--- src/Datatable/hooks/useTable.tsx
+++ src/Datatable/hooks/useTable.tsx
@@ -1,7 +1,6 @@
 import { DatatableHeaderGroups, Row, TDatatable } from '../Type'
 import { generateHeader, generateRowGroups } from '../Datatable'
-
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 /**
  * It takes in an array of columns and an array of data and returns an array of table headers and an
@@ -16,14 +15,16 @@ export function useTable({ data, columns }: TDatatable, ...hooks: any): any {
     () => generateHeader(columns),
     [columns]
   )
-
+  const [dataRow, setDataRow] = useState([])
   const { searchTherm, searchArray } = useSearch ? useSearch(data) : []
-  let { sortData } = useSort ? useSort(tableHeaders, data) : []
+  const { sortData } = useSort ? useSort(tableHeaders, data) : []
 
-  const dataRow = useMemo(() => {
-    sortData = [...searchArray]
-    return sortData
-  }, [searchArray, sortData])
+  useEffect(() => {
+    setDataRow(searchArray)
+  }, [searchArray])
+  useEffect(() => {
+    setDataRow(sortData)
+  }, [sortData])
 
   const {
     matrix,
